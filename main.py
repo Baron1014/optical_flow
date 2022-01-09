eef31e447b41a0a39b5daace046863df7607644a
Baron <04170133@gm.scu.edu.tw>
2022-01-09 21:21:12 +0800

    新增迭代結果

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 
-def main(img_path, next_img, output_name):
-     # read image & template
+def main(img_path, next_img, output_name, epochs = 20):
+    # read image & template
     img = cv2.imread(img_path)
     next_i = cv2.imread(next_img)
 
@@ -20,24 +20,33 @@ def main(img_path, next_img, output_name):
 
     # calculate optical flow
     frame_gray = cv2.cvtColor(next_i, cv2.COLOR_BGR2GRAY)
-    # Parameters for lucas kanade optical flow
-    lk_params = dict(winSize  = (15, 15),
-                    maxLevel = 2,
-                    criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
-    p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
 
-    # Select good points
-    if p1 is not None:
-        good_new = p1[st==1]
-        good_old = p0[st==1]
+    for epoch in range(1,epochs, 2):
+        # Parameters for lucas kanade optical flow
+        lk_params = dict(winSize  = (15, 15),
+                        maxLevel = 2,
+                        criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, epoch, 0.03))
+        p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
 
-    # draw the tracks   
-    for i, (new, old) in enumerate(zip(good_new, good_old)):
-        a, b = new.ravel()
-        c, d = old.ravel()       
-        cv2.circle(next_i, (int(c), int(d)), 3, (255,0,0), -1) #old
-        cv2.circle(next_i, (int(a), int(b)), 3, (0,255,0), -1) # new
-        cv2.line(next_i, (int(a), int(b)), (int(c), int(d)), (255,0,255), 2)
+        # Select good points
+        if epoch==1:
+            good_new = p1[st==1]
+            good_old = p0[st==1]
+        else:
+            good_new = p1[st==1]
+
+        # draw the tracks   
+        for i, (new, old) in enumerate(zip(good_new, good_old)):
+            a, b = new.ravel()
+            c, d = old.ravel()       
+            cv2.line(next_i, (int(a), int(b)), (int(c), int(d)), (255,0,255), 2)
+            if epoch==1:
+                cv2.circle(next_i, (int(c), int(d)), 3, (255,0,0), -1) #old
+            elif epoch==epochs-1:
+                cv2.circle(next_i, (int(a), int(b)), 3, (0,255,0), -1) # new
+            else:
+                cv2.circle(next_i, (int(a), int(b)), 3, (0,0,255), -1) # new
+            good_old = good_new
 
     # save
     save_image(next_i, f"{output_name}_track", "data/")
